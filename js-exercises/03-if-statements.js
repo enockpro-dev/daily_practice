9103ba86201f619d33e6d3ab0f97a99b8ab0d785
enockpro-dev <basistenoch@gmail.com>
2026-04-21 00:43:00 +0300

    Create 03-if-statements.js

---
+++ js-exercises/03-if-statements.js
@@ -0,0 +1,63 @@
+// If Statement Practice
+
+// Task 11: Simple if statement
+let isRaining = true;
+
+if (isRaining) {
+  console.log("Take an umbrella.");
+}
+
+// Change true to false and run again.
+
+
+// Task 12: If and else
+let isHungry = false;
+
+if (isHungry) {
+  console.log("Eat some food.");
+} else {
+  console.log("You are okay for now.");
+}
+
+// Change the value and test again.
+
+
+// Task 13: Check age
+let studentAge = 20;
+
+if (studentAge >= 18) {
+  console.log("You are an adult.");
+} else {
+  console.log("You are not an adult yet.");
+}
+
+// Try different ages.
+
+
+// Task 14: Check a number
+let number = 7;
+
+if (number > 10) {
+  console.log("The number is big.");
+} else {
+  console.log("The number is small.");
+}
+
+
+// Task 15: Check your favorite color
+let favoriteColor = "blue";
+
+if (favoriteColor === "blue") {
+  console.log("Blue is a cool color.");
+} else {
+  console.log("That is a nice color too.");
+}
+
+
+// Mini challenge 3:
+// 1. Create a variable called hasMoney
+// 2. If it is true, print "I can buy a snack"
+// 3. If it is false, print "I need to save money"
+// 4. Create a variable called score
+// 5. If score is 50 or more, print "You passed"
+// 6. Else, print "You failed"
