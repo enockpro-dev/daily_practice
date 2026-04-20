242bedc4ccd313a25dcb473299fbcb27f7219dbd
enockpro-dev <basistenoch@gmail.com>
2026-04-20 23:58:04 +0300

    Update learn .js

--- learn .js
+++ learn .js
@@ -268,3 +268,44 @@ console.log(total);
 // 3. Make a variable called sum
 // 4. Use a loop to add numbers from 1 to 3
 // 5. Print the final sum
+
+
+// ----------------------------
+// Array Practice
+// ----------------------------
+
+// Task 26: Make an array
+let fruits = ["apple", "banana", "mango"];
+console.log(fruits);
+
+
+// Task 27: Get one item from an array
+console.log(fruits[0]);
+console.log(fruits[1]);
+
+// Arrays start counting at 0.
+
+
+// Task 28: Change an item
+fruits[1] = "orange";
+console.log(fruits);
+
+
+// Task 29: Add a new item
+fruits.push("grapes");
+console.log(fruits);
+
+
+// Task 30: Loop through an array
+for (let i = 0; i < fruits.length; i++) {
+  console.log(fruits[i]);
+}
+
+
+// Mini challenge 6:
+// 1. Create an array called colors
+// 2. Put 3 colors inside it
+// 3. Print the first color
+// 4. Change the second color
+// 5. Add one more color
+// 6. Use a loop to print all colors
