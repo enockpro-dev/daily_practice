ae57a4422da39dd49e3781506c3e6f90779b9eed
enockpro-dev <basistenoch@gmail.com>
2026-04-21 00:43:16 +0300

    Create 05-loops.js

---
+++ js-exercises/05-loops.js
@@ -0,0 +1,48 @@
+// Loop Practice
+
+// Task 21: Count from 1 to 5
+for (let i = 1; i <= 5; i++) {
+  console.log(i);
+}
+
+
+// Task 22: Count from 5 to 1
+for (let i = 5; i >= 1; i--) {
+  console.log(i);
+}
+
+
+// Task 23: Print the same word many times
+for (let i = 1; i <= 3; i++) {
+  console.log("JavaScript");
+}
+
+// Change 3 to another number.
+
+
+// Task 24: Use a variable in a loop
+let message = "I am learning JS";
+
+for (let i = 1; i <= 4; i++) {
+  console.log(message);
+}
+
+
+// Task 25: Add numbers in a loop
+let total = 0;
+
+for (let i = 1; i <= 5; i++) {
+  total = total + i;
+}
+
+console.log(total);
+
+// This adds 1 + 2 + 3 + 4 + 5
+
+
+// Mini challenge 5:
+// 1. Write a loop that prints numbers from 1 to 10
+// 2. Write a loop that prints your name 5 times
+// 3. Make a variable called sum
+// 4. Use a loop to add numbers from 1 to 3
+// 5. Print the final sum
