63badc2a2ef1b7b41f72e040d8bab4e76030f02d
enockpro-dev <basistenoch@gmail.com>
2026-04-21 00:43:57 +0300

    Create 09-string-methods.js

---
+++ js-exercises/09-string-methods.js
@@ -0,0 +1,33 @@
+// String Method Practice
+
+// Task 41: Find the length of a string
+let word = "JavaScript";
+console.log(word.length);
+
+
+// Task 42: Change text to uppercase
+console.log(word.toUpperCase());
+
+
+// Task 43: Change text to lowercase
+console.log(word.toLowerCase());
+
+
+// Task 44: Check if text includes a word
+let sentence = "I am learning JavaScript";
+console.log(sentence.includes("learning"));
+console.log(sentence.includes("Python"));
+
+
+// Task 45: Get part of a string
+console.log(word.slice(0, 4));
+console.log(word.slice(4, 10));
+
+
+// Mini challenge 9:
+// 1. Create a string called school
+// 2. Print its length
+// 3. Print it in uppercase
+// 4. Print it in lowercase
+// 5. Check if it includes one letter or word
+// 6. Print one part of the string with slice()
