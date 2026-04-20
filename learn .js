cdbfde6b56fb536ec9e5a54e2bc7cc43c28b1bdd
enockpro-dev <basistenoch@gmail.com>
2026-04-20 23:58:45 +0300

    Update learn .js

--- learn .js
+++ learn .js
@@ -309,3 +309,46 @@ for (let i = 0; i < fruits.length; i++) {
 // 4. Change the second color
 // 5. Add one more color
 // 6. Use a loop to print all colors
+
+
+// ----------------------------
+// Object Practice
+// ----------------------------
+
+// Task 31: Make an object
+let person = {
+  name: "Edv",
+  age: 20,
+  country: "Uganda"
+};
+
+console.log(person);
+
+
+// Task 32: Get values from an object
+console.log(person.name);
+console.log(person.age);
+
+
+// Task 33: Change a value
+person.age = 21;
+console.log(person.age);
+
+
+// Task 34: Add a new value
+person.favoriteFood = "Rice";
+console.log(person.favoriteFood);
+
+
+// Task 35: Use object values in a sentence
+console.log(person.name + " is " + person.age + " years old.");
+console.log(person.name + " lives in " + person.country + ".");
+
+
+// Mini challenge 7:
+// 1. Create an object called car
+// 2. Give it these values: brand, color, year
+// 3. Print the brand
+// 4. Change the color
+// 5. Add a new value called owner
+// 6. Print one full sentence about the car
