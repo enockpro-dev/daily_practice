6087ae00e3a352575d81049fcc61d927b2d87a3c
enockpro-dev <basistenoch@gmail.com>
2026-04-21 00:43:24 +0300

    Create 06-arrays.js

---
+++ js-exercises/06-arrays.js
@@ -0,0 +1,37 @@
+// Array Practice
+
+// Task 26: Make an array
+let fruits = ["apple", "banana", "mango"];
+console.log(fruits);
+
+
+// Task 27: Get one item from an array
+console.log(fruits[0]);
+console.log(fruits[1]);
+
+// Arrays start counting at 0.
+
+
+// Task 28: Change an item
+fruits[1] = "orange";
+console.log(fruits);
+
+
+// Task 29: Add a new item
+fruits.push("grapes");
+console.log(fruits);
+
+
+// Task 30: Loop through an array
+for (let i = 0; i < fruits.length; i++) {
+  console.log(fruits[i]);
+}
+
+
+// Mini challenge 6:
+// 1. Create an array called colors
+// 2. Put 3 colors inside it
+// 3. Print the first color
+// 4. Change the second color
+// 5. Add one more color
+// 6. Use a loop to print all colors
