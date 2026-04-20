aaa98a907ac9656330f069ac83f3572d632bb4f0
enockpro-dev <basistenoch@gmail.com>
2026-04-21 00:43:34 +0300

    Create 08-array-methods.js

---
+++ js-exercises/08-array-methods.js
@@ -0,0 +1,37 @@
+// Array Method Practice
+
+// Task 36: Find how many items are in an array
+let animals = ["dog", "cat", "cow", "goat"];
+console.log(animals.length);
+
+
+// Task 37: Add an item at the end
+animals.push("sheep");
+console.log(animals);
+
+
+// Task 38: Remove the last item
+animals.pop();
+console.log(animals);
+
+
+// Task 39: Check if an array has a value
+console.log(animals.includes("cat"));
+console.log(animals.includes("lion"));
+
+// This prints true or false.
+
+
+// Task 40: Join array items into one string
+let animalText = animals.join(", ");
+console.log(animalText);
+
+
+// Mini challenge 8:
+// 1. Create an array called foods
+// 2. Put 4 foods inside it
+// 3. Print the length of the array
+// 4. Add one more food
+// 5. Remove the last food
+// 6. Check if one food is inside the array
+// 7. Join all foods into one line and print it
